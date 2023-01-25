95556b8a9ecee5822a2974a384f9a4d8720d759e
Rendy Setiawan <lierendysetiawan3232@gmail.com>
2023-01-25 22:56:24 +0800

    Revised

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,7 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px'
-    },
-    extend: {
-      fontFamily: {
-        sans: ['Josefin Sans', 'sans-serif'],
-        alata: ['Alata']
-      },
-      letterSpacing: {
-        widest: '.3em'
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
